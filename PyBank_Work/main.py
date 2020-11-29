af4bdf193ac3fb9d41245a37daf5e231a9f64ab7
unknown <noahdgoldstein@gmail.com>
2020-11-29 16:50:27 -0500

    updating python hw code

--- PyBank_Work/main.py
+++ PyBank_Work/main.py
@@ -1,35 +1,82 @@
-# PyBank Code
 import os
 import csv
 
-budget_data_csv = os.path.join("Resources","budget_data.csv")
+budget_data_csv = os.path.join('Resources','budget_data.csv')
 
-# Define the function
-def budget_calc(budget_data):
-    date = str(budget_data[0])
-    profit_losses = int(budget_data[1])
+# 
+total_months = 0
 
+net_ProfitLoss = 0
 
+# Creates blank list for average changes in profits and losses
+average_change_ProfitLoss = []
 
+prior_month = 0
 
+# greatest_increase = {
+#     "month": " ",
+#     "profit": -1000
+# }
+# greatest_decrease = {
+#      "month": " ",
+#     "profit": 1000
+# }
 
+greatest_monthly_profit = {"profit": -1000}
+lowest_monthly_profit = {"profit": 1000}
+greatest_month = {"month": " "}
+lowest_month = {"month": " "}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-with open(budget_data_csv, 'r') as csvfile:
+with open(budget_data_csv, newline="") as csvfile:
+    
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
+    
+    #row represents a list
     for row in csvreader:
+        current_month = int(row[1])
+        if total_months > 0:
+            #this will return false the first time because its the first item in the column, first item not greater than first item in row[1]
+            total_month_change = current_month - prior_month
+            average_change_ProfitLoss.append(total_month_change)
+            if greatest_monthly_profit["profit"] < total_month_change:
+                greatest_monthly_profit["profit"] = total_month_change
+                greatest_month["month"] = row[0]
+            if lowest_monthly_profit["profit"] > total_month_change:
+                lowest_monthly_profit["profit"] = total_month_change
+                lowest_month["month"] = row[0]
+            # if greatest_increase["profit"] < total_month_change:
+            #     greatest_increase["profit"] = total_month_change
+            #     greatest_increase["month"] = row[0]
+            # if greatest_decrease["profit"] > total_month_change:
+            #     greatest_decrease["profit"] = total_month_change
+            #     greatest_decrease["month"] = row[0]
+        #sets previous month from 0 to row 1
+        prior_month = current_month
+        total_months = total_months + 1
+        #converting from str to int and redefine the amoutnt of net total amount
+        #taking current value of net total amount and adding the row value to it which becomes the new net total amount
+        net_ProfitLoss = net_ProfitLoss + current_month
+    #this is the average formula for the average of the list called average change
+    month_average = sum(average_change_ProfitLoss)/len(average_change_ProfitLoss)
+
+#print statements
+print(f'Financial Analysis')
+print(f'-------------------------------')
+print(f'Total Months: {total_months}')
+print(f'Total: ${net_ProfitLoss}')
+print(f'Average Change: ${month_average:.2f}') #two decimal points
+print(f'Greatest Increase in Profits: {greatest_month["month"]} (${greatest_monthly_profit["profit"]})')
+print(f'Greatest Decrease in Profits: {lowest_month["month"]} (${lowest_monthly_profit["profit"]})')
+
+# #Text file output
+# pybank_text_file = os.path.join('Analysis', 'pybank_output_text_file.txt')
+# with open(pybank_text_file, 'w') as textfile:
+#     textwriter = textfile.write(f'Financial Analysis\n')
+#     textwriter = textfile.write(f'-------------------------------\n')
+#     textwriter = textfile.write(f'Total Months: {month_count}\n')
+#     textwriter = textfile.write(f'Total: ${net_total_amount}\n')
+#     textwriter = textfile.write(f'Average Change: ${month_average:.2f}\n')
+#     textwriter = textfile.write(f'Greatest Increase in Profits: {greatest_month["month"]} (${greatest_monthly_profit["profit"]})\n')
+#     textwriter = textfile.write(f'Greatest Decrease in Profits: {lowest_month["month"]} (${lowest_monthly_profit["profit"]})\n')
--- PyPoll_Work/main.py
+++ PyPoll_Work/main.py
@@ -3,27 +3,27 @@ import csv
 
 election_data_csv = os.path.join('Resources', 'election_data.csv')
 
+# Sets 'total' variable equal to 0 to start
 total = 0
 
-# create blank list for candidates
+# Create blank list for candidates
 candidates = []
 
-# create blank dictionary for candidates and their votes
+# Create blank dictionary for candidates and their votes
 candidate_votes = {}
 
-# Using function to calculate percentage votes
-# def percent_votes(votes):
-#     percent = round((votes/total) * 100,2)
-#     return percent
+# Sets 'election_winner_votes' variable as 0 to start
+election_winner_votes = 0
 
-election_winner = 0
-
+# Beginning of code to determine HW outputs
 with open(election_data_csv, newline="") as csvfile:
    
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
     header = next(csvreader)
    
+    # Code to calculate the total number of votes, set the unique list of 'candidates', 
+    # and create a dictionary for each candidate and their corresponding number of votes
     for row in csvreader:
         total = total + 1
         candidate = row[2]
@@ -32,39 +32,27 @@ with open(election_data_csv, newline="") as csvfile:
             candidate_votes[candidate]=0
         candidate_votes[candidate] = candidate_votes[candidate] + 1
 
-# create an export to txt file for this output "with open (the path)" : https://www.geeksforgeeks.org/writing-csv-files-in-python/
-
-
 print(f"Election Results\n"
 f"-------------------------\n"
 f"Total Votes: {total}\n"
 f"-------------------------")
 
-# Using loop to calculate values
+# Using loop to calculate percent values for each candidate and determine the winning candidate
 for candidate in candidates:
     votes = candidate_votes[candidate]
     percent = (votes/total) * 100
-    if votes > election_winner:
-        election_winner = votes
+    if votes > election_winner_votes:
+        election_winner_votes = votes
         winner = candidate
     print(f"{candidate}: {percent:.3f}% ({votes})")
 print(f"-------------------------\nWinner: {winner}\n-------------------------")
 
-
-# Election Results
-# -------------------------
-# Total Votes: 3521001
-# -------------------------
-# Khan: 63.000% (2218231)
-# Correy: 20.000% (704200)
-# Li: 14.000% (492940)
-# O'Tooley: 3.000% (105630)
-# -------------------------
-# Winner: Khan
-# -------------------------
-
-    # print(percent_votes(votes)) ...referring to function defined above
-
-# print(candidates, candidate_votes)
-# print(percent_votes(candidate_votes["Khan"]))
-# print (f'Total votes cast were {total}')
+# create an export to txt file for this output "with open (the path)" : 
+# Text file output
+# pypoll_text_file = os.path.join('Analysis', 'pypoll_output_text_file.txt')
+# with open(pypoll_text_file, 'w') as textfile:
+#     textwriter = textfile.write(f'-------------------------\n')
+#     textwriter = textfile.write(f'Total votes {total}\n')
+#     # for x in range(len(candidate_name)):
+#     textwriter = textfile.write(f"{candidate_name}: {percent_candidate_won:.3f}% {votes_received}\n")
+#     textwriter = textfile.write(f"-------------------------\nWinner: {election_winner}\n-------------------------\n")
